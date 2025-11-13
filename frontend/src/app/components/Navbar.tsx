9487b4757843249d4b3a3febcdb834c4a501ac21
Roshan Kewat <roshankewat9090@gmail.com>
2025-11-14 01:51:13 +0530

    navbar completed.

--- frontend/src/app/components/Navbar.tsx
+++ frontend/src/app/components/Navbar.tsx
@@ -1,6 +1,7 @@
 'use client';
 import React from "react";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 interface ILink {
     path: string;
@@ -8,6 +9,9 @@ interface ILink {
 }
 
 const Navbar = () => {
+    const omittedPathnames = ['/auth/signin', '/auth/signup'];
+    const pathname = usePathname();
+
     const links: ILink[] = [
         {
             path: '/developers',
@@ -24,7 +28,23 @@ const Navbar = () => {
     ]
 
   return (
-    <></>
+    <nav className={`w-[100vw] h-[8vh] bg-white shadow-md ${omittedPathnames.includes(pathname)?'hidden':'flex'} items-center justify-between px-8`}>
+        <span className="font-medium text-lg">devlopsfolio</span>
+
+        <div className="flex items-center justify-center gap-5">
+            {links.map((link, index) => (
+                <Link key={index} href={link.path} className="text-black hover:text-gray-900 hover:font-medium transition-all duration-200">
+                    {link.pathName}
+                </Link>
+            ))}
+        </div>
+
+        <div className="flex items-center justify-center gap-3">
+            <Link href={'/auth/signup'} className="text-black hover:text-white rounded-lg hover:bg-gray-900 transition-all duration-500 py-1 px-5">Sign Up</Link>
+            
+            <Link href={'/auth/signin'} className="bg-black text-white rounded-lg hover:bg-gray-900 transition-all duration-500 py-1 px-5">Sign In</Link>
+        </div>
+    </nav>
   )
 };
 
--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,7 +1,17 @@
-import Link from "next/link";
-
 export default function Home() {
   return (
-    <></>
+    <>
+    <main className="w-full h-fit py-12 flex flex-col items-center justify-center gap-12">
+      <div className="flex flex-col items-center justify-center gap-4 select-none">  
+        <span className="font-extrabold text-8xl">Showcase Your Skills</span>
+        <span className="font-extrabold text-8xl">Get Hired</span>
+      </div>
+
+      <div className="flex items-center justify-center gap-5">
+        <button className="bg-black text-white rounded-xl w-72 py-2 hover:bg-gray-900 transition-all duration-500 cursor-pointer shadow-md outline-none">Join as Developer</button>
+        <button className="bg-gray-900 text-white rounded-xl w-72 py-2 hover:bg-gray-900 transition-all duration-500 cursor-pointer shadow-md outline-none">Hire Developer</button>
+      </div>
+    </main>
+    </>
   );
 }
